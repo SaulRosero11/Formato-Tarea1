c9f6b2fd669eca73bcb43dea6e10338732b71244
Saul Rosero <anthonyrosero01@gmail.com>
2025-04-16 23:17:30 -0500

    Comentarios

--- Tarea-1/Program.cs
+++ Tarea-1/Program.cs
@@ -24,10 +24,19 @@ class Program
       {
         case "1":
           // Ejecutamos el Ejercicio1
+          /*
+          En un archivo tenemos las notas de un grupo de alumnos de una clase con valores Nota(1), Nota(2), ..., Nota(n – 1), establecidas entre 0 y 10.
+          El último dato es un dato con valor – 50. El número de notas que contiene el fichero no se conoce, pero se sabe que el mínimo número de alumnos
+          en la clase es 1 y el máximo 100. Se desea crear un programa que determine la nota media.
+          */
           Ejercicio1.Menu();
           break;
         case "2":
           // Ejecutamos el Ejercicio2
+          /*
+          Crea un programa en C que pida una cadena al usuario y muestre por pantalla la cadena invertida. Por ejemplo, si la cadena introducida es calor,
+          deberá mostrarse por pantalla rolac. Si la cadena introducida es uisrael.edu.ec deberá mostrarse por pantalla ce.ude.learsiu
+          */
           Ejercicio2.Menu();
           break;
         case "0":
