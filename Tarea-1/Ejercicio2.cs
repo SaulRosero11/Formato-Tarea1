4476663fbc35d47f7148e4771c3bd8786585d446
Saul Rosero <anthonyrosero01@gmail.com>
2025-04-19 00:45:59 -0500

    Final de la tarea

--- Tarea-1/Ejercicio2.cs
+++ Tarea-1/Ejercicio2.cs
@@ -8,13 +8,14 @@ public static class Ejercicio2
 {
   public static void Menu()
   {
+    // Variable para controlar el ciclo del menu
     bool start = true;
 
     while (start)
     {
       Console.Clear();
       Console.WriteLine("Menu 'Ejercicio 2'");
-      Console.WriteLine("1. Ingresar texto");
+      Console.WriteLine("1. Ingresar una palabra para invertir los caracteres");
       Console.WriteLine("0. Regresar al menu principal");
       Console.WriteLine("Selecciona una opción:");
 
@@ -23,8 +24,14 @@ public static class Ejercicio2
       switch (option)
       {
         case "1":
+          // Llamamos al metodo privado Ejecutar
+          Execute();
+          Console.WriteLine("Presiona [Enter] para continuar.");
+          Console.ReadLine();
           break;
         case "0":
+          // Finalizamos el ciclo y regresamos al menu principal
+          start = false;
           break;
         default:
           Console.WriteLine("Opción no válida. Presiona [Enter] para intentar nuevamente.");
@@ -33,4 +40,54 @@ public static class Ejercicio2
       }
     }
   }
+
+  private static void Execute() {
+
+    // Inciamos las variables
+    int counter = 0;
+    string word = "";
+    string invertedWord;
+
+    // Ingresamos un caracter y validamos hasta que ingrese un caracter valido al menos 2 caracteres para ver la funcionalidad
+    while(word.Length < 2) {
+      Console.Clear();
+      if(counter > 0) {
+        Console.WriteLine("Ingresa una palabra valida, debe tener al menos 2 caracteres:");
+      } else {
+        Console.WriteLine("Ingresa una frase:");
+      }
+
+      word = Console.ReadLine() ?? "";
+      counter ++;
+    }
+
+    try {
+
+      // Ejecucion de funcion para invertir la palabra
+      invertedWord = InvestWord(word);
+
+      // Imprimimos el resultado
+      Console.Clear();
+      Console.WriteLine($"Palabra ingresada: {word}");
+      Console.WriteLine($"Palabra invertida: {invertedWord}");
+
+    } catch (Exception ex)
+    {
+      Console.WriteLine($"Error : {ex.Message}");
+    }
+  }
+
+  private static string InvestWord(string word){
+    
+    // Convertimos la palabra en un arrego de caracteres
+    char[] arrayWord = word.ToCharArray();
+    // Usamos el array reverse para revertir el arreglo y conseguir el efecto de invertir la palabra
+    Array.Reverse(arrayWord);
+
+    // Guardamos en un string el arreglo
+    string result = new string(arrayWord);
+
+    return result;
+  }
+
 }
--- Tarea-1/obj/Debug/net8.0/Tarea-1.AssemblyInfo.cs
+++ Tarea-1/obj/Debug/net8.0/Tarea-1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9f6b2fd669eca73bcb43dea6e10338732b71244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d955aa813f3ac888d1fa12d879ed8e82fed71c7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
